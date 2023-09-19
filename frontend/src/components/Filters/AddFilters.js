bd3ffbb33947915838265007cf2a6621d9bda156
Marcel <marcelnedza2000@gmail.com>
2023-09-19 10:57:58 +0200

    added button with styles in Search componenent for showing AddFilters Component (without logic yet)

--- frontend/src/components/Filters/AddFilters.js
+++ frontend/src/components/Filters/AddFilters.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AddFilters.module.css";
 
 const AddFilters = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleFiltersVisibility = (e) => {
+    e.preventDefault();
+
+    console.log(visible);
+
+    setVisible(!visible);
+  };
+
   return (
-    <div className={classes["add-filters"]}>
+    <div
+      className={`${classes["add-filters"]} ${
+        visible && classes["show-filters"]
+      }`}
+    >
       <div className={classes["add-filters-content"]}>
         <header className={classes.header}>
-            Filters
-            <button className={classes["close-button"]}> &times;</button>
-            </header>
+          <h2 className={classes.title}>Filters</h2>
+          <button
+            className={classes["close-button"]}
+            onClick={toggleFiltersVisibility}
+          >
+            &times;
+          </button>
+        </header>
       </div>
     </div>
   );
 };
 
 export default AddFilters;
-
--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -4,12 +4,13 @@ import styles from "./Search.module.css";
 const Search = () => {
   return (
     <section className={styles.search}>
-      <div className={`${styles['search-content']} wrapper`}>
+      <div className={`${styles["search-content"]} wrapper`}>
         <h1 className={styles.title}>Search for a recipe</h1>
-        <input className={styles.input} placeholder="Search"/>
+        <input className={styles.input} placeholder="Search" />
+        <button className={styles.button}>Add Filters</button>
       </div>
     </section>
   );
-}
+};
 
 export default Search;

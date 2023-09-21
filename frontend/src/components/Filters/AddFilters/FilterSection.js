c8f44e011ea168a66f2ef10814bb106204b5244d
Marcel <marcelnedza2000@gmail.com>
2023-09-21 11:47:51 +0200

    added styling and unstyling elements in AddFilters when clicked

--- frontend/src/components/Filters/AddFilters/FilterSection.js
+++ frontend/src/components/Filters/AddFilters/FilterSection.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AddFilters.module.css";
 import styles from "./Elements.module.css";
 
 const FilterSection = (props) => {
+  const [choosenElements, setChoosenElements] = useState(
+    new Array(props.items.length).fill(false)
+  );
+
+  const handleElement = (index) => {
+    const updatedChoosenElements = [...choosenElements];
+    updatedChoosenElements[index] = !choosenElements[index];
+    setChoosenElements(updatedChoosenElements);
+  };
+
   return (
     <section className={classes.section}>
       <h3 className={classes["section-title"]}>{props.title}</h3>
       <ul className={styles.ul}>
-        {props.items.map((item) => (
-          <li className={styles.li}>{item}</li>
+        {props.items.map((item, index) => (
+          <li
+            key={index}
+            className={`${styles.li} ${
+              choosenElements[index] && styles.choosen
+            }`}
+            onClick={() => handleElement(index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </section>
